234c3604770b35102b350243345cebec6c4b57e2
13554932803 <F053886>
2019-06-10 14:54:18 +0800

    歌单列表完成

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper" v-show="songs.length > 0">
+        <div class="play" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -19,6 +25,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
@@ -26,6 +35,11 @@
 <script>
 import SongList from 'base/song-list/song-list'
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 const RESERVED_HEIGHT = 40
 export default {
@@ -45,7 +59,8 @@ export default {
   },
   components: {
     SongList,
-    Scroll
+    Scroll,
+    Loading
   },
   data() {
     return {
@@ -67,6 +82,9 @@ export default {
     scroll(pos) {
       // console.log(pos.y)
       this.scrollY = pos.y
+    },
+    back() {
+      this.$router.back()
     }
   },
   watch: {
@@ -80,8 +98,8 @@ export default {
       // console.log('minTranslateY:' + this.minTranslateY)
       // console.log('tranlateY:' + tranlateY)
       // 这里是组件节点
-      this.$refs.layer.style['transform'] = `translate3d(0, ${tranlateY}px, 0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${tranlateY}px, 0)`
+      this.$refs.layer.style[transform] = `translate3d(0, ${tranlateY}px, 0)`
+      // this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${tranlateY}px, 0)`
       const percent = Math.abs(newY / this.imageHeight)
       if (newY > 0) {
         zIndex = 10
@@ -89,18 +107,20 @@ export default {
       } else {
         blur = Math.min(20 * percent, 20)
       }
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur})px`
-      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur})px`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+      // this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = '40px'
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+      // this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
       this.$refs.bgImage.style.zIndex = zIndex
     }
   },
@@ -152,6 +172,30 @@ export default {
       padding-top 70%
       transform-origin top
       background-size cover
+      .play-wrapper
+        position absolute
+        bottom 20px
+        z-index 50
+        width 100%
+        .play
+          box-sizing border-box
+          width 135px
+          padding 7px 0
+          margin 0 auto
+          text-align center
+          border 1px solid $color-theme
+          color $color-theme
+          border-radius 100px
+          font-size 0
+          .icon-play
+            display inline-block
+            vertical-align middle
+            margin-right 6px
+            font-size $font-size-medium-x
+          .text
+            display inline-block
+            vertical-align middle
+            font-size $font-size-small
       .filter
         position absolute
         top 0
@@ -172,4 +216,9 @@ export default {
       background: $color-background
       .song-list-wrapper
         padding 20px 30px
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
 </style>
